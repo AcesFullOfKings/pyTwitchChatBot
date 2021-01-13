b6033300c416d87d0a39713cd4293b4bc282b524
theonefoster <6105690+theonefoster@users.noreply.github.com>
2021-01-13 16:33:38 +0000

    removed test code

--- chatbot.py
+++ chatbot.py
@@ -161,107 +161,103 @@ class ChatBot():
 
 		messages = []
 
-		for line in lines:
-			try:
-				if self.debug:
-					print(line)
+	for line in lines:
+			if self.debug:
+				print(line)
 
-				if line[:4] == "PING":
-					self.send_pong()
-					continue
+			if line[:4] == "PING":
+				self.send_pong()
+				continue
 
-				"""
-				as I'm using "if x in line" to detect message types, in theory someone could send a message in chat
-				saying e.g. "hey guys tmi.twitch.tv NOTICE #", so that line would have both PRIVMSG and (e.g.) NOTICE
-				in it. By checking for PRIVMSG first, no matter the content of the message it will always be processed
-				as a chat message. Kind of like sanitising it. Sort of.
-				"""
+			"""
+			as I'm using "if x in line" to detect message types, in theory someone could send a message in chat
+			saying e.g. "hey guys tmi.twitch.tv NOTICE #", so that line would have both PRIVMSG and (e.g.) NOTICE
+			in it. By checking for PRIVMSG first, no matter the content of the message it will always be processed
+			as a chat message. Kind of like sanitising it. Sort of.
+			"""
 
-				message_dict = None # will be set to a dict below if message type is recognised
+			message_dict = None # will be set to a dict below if message type is recognised
 
-				if "tmi.twitch.tv PRIVMSG #" in line: # chat message from user (other message types are possible e.g. NOTICE)
-					message_dict = {"message_type":"privmsg"}
+			if "tmi.twitch.tv PRIVMSG #" in line: # chat message from user (other message types are possible e.g. NOTICE)
+				message_dict = {"message_type":"privmsg"}
 
-					try:
-						if "tags" in self.granted_capabilities and line[0] == "@":
-							line = line[1:] # remove the @ from the beginning
+				try:
+					if "tags" in self.granted_capabilities and line[0] == "@":
+						line = line[1:] # remove the @ from the beginning
 
-							# first section is all tags except last one, then we add the last one to the list by a different method
-							msg_tags = (";".join(line.split("PRIVMSG")[0].split(";")[:-1]) + ";" + (line.split("PRIVMSG")[0].split(":")[-2]).split(";")[-1]).split(";") # dumb that I have to do this
-							for tag in msg_tags:
-								if "=" in tag:
-									key, val = tag.split("=")
-									message_dict[key.strip()] = val.strip()
+						# first section is all tags except last one, then we add the last one to the list by a different method
+						msg_tags = (";".join(line.split("PRIVMSG")[0].split(";")[:-1]) + ";" + (line.split("PRIVMSG")[0].split(":")[-2]).split(";")[-1]).split(";") # dumb that I have to do this
+						for tag in msg_tags:
+							if "=" in tag:
+								key, val = tag.split("=")
+								message_dict[key.strip()] = val.strip()
+					else:
+						start_of_name = line.index(":") + 1
+						end_of_name = line.index("!")
+						username = line[start_of_name:end_of_name].lower()
+						if 4 <= len(username) <= 25: # min and max lengths for username.. just a sanity check
+							message_dict["display-name"] = username
 						else:
-							start_of_name = line.index(":") + 1
-							end_of_name = line.index("!")
-							username = line[start_of_name:end_of_name].lower()
-							if 4 <= len(username) <= 25: # min and max lengths for username.. just a sanity check
-								message_dict["display-name"] = username
-							else:
-								raise ValueError("Unable to find username in line.")
-						message_dict["message"] = ":".join(line.split("PRIVMSG")[1].split(":")[1:]) # everything after the PRIVMSG.. then after the subsequent colon
-					except (ValueError, IndexError):
-						if self.debug:
-							print("Unable to parse line as message.")
-						continue # bad line
+							raise ValueError("Unable to find username in line.")
+					message_dict["message"] = ":".join(line.split("PRIVMSG")[1].split(":")[1:]) # everything after the PRIVMSG.. then after the subsequent colon
+				except (ValueError, IndexError):
+					if self.debug:
+						print("Unable to parse line as message.")
+					continue # bad line
 
-				elif "tmi.twitch.tv NOTICE #" in line: # chat message from user (other message types are possible e.g. NOTICE)
-					"""@msg-id=color_changed :tmi.twitch.tv NOTICE #kaywee :Your color has been changed."""
-					message_dict = {"message_type":"notice"}
-					line = line[1:] # remove the @ from the beginning
-					message_dict["msg_id"]  = line.split(":")[0][:-1].split("=")[1]
-					message_dict["message"] = line.split(":")[-1]
+			elif "tmi.twitch.tv NOTICE #" in line: # chat message from user (other message types are possible e.g. NOTICE)
+				"""@msg-id=color_changed :tmi.twitch.tv NOTICE #kaywee :Your color has been changed."""
+				message_dict = {"message_type":"notice"}
+				line = line[1:] # remove the @ from the beginning
+				message_dict["msg_id"]  = line.split(":")[0][:-1].split("=")[1]
+				message_dict["message"] = line.split(":")[-1]
 
-				elif "tmi.twitch.tv USERNOTICE #" in line:
-					message_dict = {"message_type":"usernotice"}
-					line = line[1:]
-					msg_tags = (";".join(line.split("USERNOTICE")[0].split(";")[:-1]) + ";" + (line.split("USERNOTICE")[0].split(":")[-2]).split(";")[-1]).split(";") # dumb that I have to do this
+			elif "tmi.twitch.tv USERNOTICE #" in line:
+				message_dict = {"message_type":"usernotice"}
+				line = line[1:]
+				msg_tags = (";".join(line.split("USERNOTICE")[0].split(";")[:-1]) + ";" + (line.split("USERNOTICE")[0].split(":")[-2]).split(";")[-1]).split(";") # dumb that I have to do this
 
+				for tag in msg_tags:
+					if "=" in tag:
+						key, val = tag.split("=")
+						message_dict[key.strip()] = val.strip()
+
+			elif ":tmi.twitch.tv USERSTATE" in line:
+				message_dict = {"message_type":"userstate"}
+
+			elif ":tmi.twitch.tv HOSTTARGET" in line:
+				target = line.split(":")[2].split(" ")[0]
+				if target not in ["-", ""]:
+					viewers = line.split(":")[2].split(" ")[1]
+					message_dict = {"message_type":"hosttarget", "host_target": target, "viewers": viewers}
+
+			elif ":tmi.twitch.tv RECONNECT" in line:
+				message_dict = {"message_type":"reconnect"}
+
+			elif ":tmi.twitch.tv CLEARMSG" in line: # single message was deleted
+				message_dict = {"message_type":"clearmsg"} 
+				if "tags" in self.granted_capabilities and line[0] == "@":
+					msg_tags = line[1:].split(":tmi.twitch.tv CLEARMSG")[0].split(";")
 					for tag in msg_tags:
 						if "=" in tag:
 							key, val = tag.split("=")
 							message_dict[key.strip()] = val.strip()
 
-				elif ":tmi.twitch.tv USERSTATE" in line:
-					message_dict = {"message_type":"userstate"}
+			elif ":tmi.twitch.tv CLEARCHAT" in line: # clear all messages from user
+				message_dict = {"message_type":"clearchat"}
+				if "tags" in self.granted_capabilities and line[0] == "@":
+					msg_tags = line[1:].split(":tmi.twitch.tv CLEARCHAT")[0].split(";")
+					for tag in msg_tags:
+						if "=" in tag:
+							key, val = tag.split("=")
+							message_dict[key.strip()] = val.strip()
 
-				elif ":tmi.twitch.tv HOSTTARGET" in line:
-					target = line.split(":")[2].split(" ")[0]
-					if target not in ["-", ""]:
-						viewers = line.split(":")[2].split(" ")[1]
-						message_dict = {"message_type":"hosttarget", "host_target": target, "viewers": viewers}
-
-				elif ":tmi.twitch.tv RECONNECT" in line:
-					message_dict = {"message_type":"reconnect"}
-
-				elif ":tmi.twitch.tv CLEARMSG" in line: # single message was deleted
-					message_dict = {"message_type":"clearmsg"} 
-					if "tags" in self.granted_capabilities and line[0] == "@":
-						msg_tags = line[1:].split(":tmi.twitch.tv CLEARMSG")[0].split(";")
-						for tag in msg_tags:
-							if "=" in tag:
-								key, val = tag.split("=")
-								message_dict[key.strip()] = val.strip()
-
-				elif ":tmi.twitch.tv CLEARCHAT" in line: # clear all messages from user
-					message_dict = {"message_type":"clearchat"}
-					if "tags" in self.granted_capabilities and line[0] == "@":
-						msg_tags = line[1:].split(":tmi.twitch.tv CLEARCHAT")[0].split(";")
-						for tag in msg_tags:
-							if "=" in tag:
-								key, val = tag.split("=")
-								message_dict[key.strip()] = val.strip()
-
-				else:
-					with open("verbose log.txt", "a", encoding="utf-8") as f:
-						f.write("Unrecognised line received in Chatbot: " + str(line) + "\n\n")
-
-				if message_dict is not None:
-					messages.append(message_dict)
-			except Exception as ex:
+			else:
 				with open("verbose log.txt", "a", encoding="utf-8") as f:
-					f.write("EXCEPTION in Chatbot: " + str(ex) + "\n\n")
+					f.write("Unrecognised line received in Chatbot: " + str(line) + "\n\n")
+
+			if message_dict is not None:
+				messages.append(message_dict)
 
 		return messages
 
